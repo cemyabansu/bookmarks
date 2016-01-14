689a114eff9d7fecd3a2a43e67dd4ea2cdec9a2c
cemyabansu <cemyabansu@hotmail.com>
2016-01-15 00:55:57 +0200

    Getting folders, adding folders

--- public/scripts/bookmarks.js
+++ public/scripts/bookmarks.js
@@ -1,3 +1,58 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
+
+  $("#folderList .collection-item").click(function() {
+    alert( "Handler for .click() called." );
+  });
+
+  //loading folders
+  LoadFolders();
 });
+
+function LoadFolders(){
+  $.get("api/folder/getall", {userid : "5684660045cf80ea12073983"})
+  .done(function( folders ) {
+    //add each folder to list
+    for (var i = 0; i < folders.length; i++) {
+      var folderList = $('#folderList');
+
+      folderList.append(
+        $('<li>').attr('class','collection-item').append(
+          $('<span>').append(folders[i].name)
+        ).bind('click', OnFolderClick)
+      );
+    }
+  }).fail(function() {
+    alert( "error" );
+  });
+}
+
+function OnFolderClick(){
+  $('#folderList .active').removeClass('active');
+  $(this).addClass('active');
+}
+
+function HandleAddFolder(){
+  $('#modal1').openModal();
+}
+
+function HandleAddItem(){
+  $('#modal1').openModal();
+}
+
+function SubmitAFolder(){
+  var folderName = $('#AddFolder_folderName').val();
+
+  var folderList = $('#folderList');
+
+  folderList.append(
+    $('<li>').attr('class','collection-item').append(
+      $('<span>').append(folderName)
+    ).bind('click', OnFolderClick)
+  );
+
+  $('#AddFolder_folderName').val('');
+
+  $('#modal1').closeModal();
+  $('#fixed_addbutton').toggleClass('active');
+}
